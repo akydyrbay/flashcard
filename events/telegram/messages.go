c9012105a57b0346257a647e638a44cf6d5c97e7
Abdulmalik <akdrbb@gmail.com>
2025-06-19 22:58:14 +0500

    Update messages.go

--- events/telegram/messages.go
+++ events/telegram/messages.go
@@ -2,16 +2,24 @@ package telegram
 
 const (
 	msgUnknownCommand = "Sorry, I didn't understand that. Type /help for usage."
-	msgHelp           = `I can save and retrieve named texts:
-/save <name> <text> - save a text under a name
-/get <name>        - retrieve saved text
+	msgHelp           = `Usage:
+/save - save a flashcards under a name
+/get - retrieve saved text
 /list - list all saved names
+/help - for usage
+/delete - to delete saved cards
+/next - to show answer
 `
-	msgHello         = "Welcome! Use /help to see commands."
-	msgAlreadyExists = "An entry with that name already exists."
-	msgSaved         = "Saved!"
-	msgNoSavedItems  = "No entries found by that name."
-	msgUsageSave     = "Usage: /save <name> q:<question1> a:<answer1> q:<question2> a:<answer2> ..."
-	msgUsageGet      = "Usage: /get <name>"
-	msgInvalidFormat = "Invalid format! Use: /save <name> q:<question1> a:<answer1> [q:<question2> a:<answer2> ...]"
+	msgHello           = "Welcome! Use /help to see commands."
+	msgAlreadyExists   = "An entry with that name already exists."
+	msgSaved           = "Saved!"
+	msgNoSavedItems    = "No entries found by that name."
+	msgUsageGet        = "Usage: /get"
+	msgInvalidFormat   = "Invalid format!"
+	msgSaveCmdResponse = "Great! Please send your Q&A in this format:\n" +
+		"q:<question1> \n a:<answer1> \n q:<question2> \n a:<answer2> ..."
+	msgNoSuchItem     = "I couldn't find a flashcards by that name."
+	msgDeleteResponse = "Sure! Please send me the name of the flashcard you want to delete."
+	msgGetCmdResponse = "Please send a name of the flashcards to get."
+	msgQuizComplete   = "Quiz is finished"
 )
